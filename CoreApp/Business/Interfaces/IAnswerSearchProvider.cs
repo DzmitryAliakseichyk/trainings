456cdad7a5b81ed7cc811f37f7c123a5f024097f
Dmitry Aliakseichyk <dmitry.aliakseichyk@itechart-group.com>
2018-08-24 17:32:10 +0300

    - add NEST
     - add base model
     - add IAnswerSearchProvider intefaces

---
+++ CoreApp/Business/Interfaces/IAnswerSearchProvider.cs
@@ -0,0 +1,9 @@
+﻿using Business.Models;
+
+namespace Business.Interfaces
+{
+    public interface IAnswerSearchProvider
+    {
+        void AddAnswer(QuestionAnswerModel questionAnswerModel);
+    }
+}
---
+++ CoreApp/Business/Models/QuestionAnswerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Business.Models
+{
+    public class QuestionAnswerModel
+    {
+        public Guid CustomerId { get; set; }
+
+        public Guid FormId { get; set; }
+
+        public Guid AuditId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Question { get; set; }
+
+        public string KeyPhrazes { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
